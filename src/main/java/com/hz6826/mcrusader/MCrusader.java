00e0d928b7a2b9db111930ecbac468796b7538e9
Hz6826 <hz_2007@qq.com>
2024-07-19 16:52:56 +0800

    icons, configures, etc.

--- src/main/java/com/hz6826/mcrusader/MCrusader.java
+++ src/main/java/com/hz6826/mcrusader/MCrusader.java
@@ -1,15 +1,15 @@
-package com.example;
+package com.hz6826.mcrusader;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class MCrusader implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    public static final Logger LOGGER = LoggerFactory.getLogger("mcrusader");
 
 	@Override
 	public void onInitialize() {
@@ -17,6 +17,7 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("MCrusader is loaded!");
+		LOGGER.info("GitHub: https://github.com/hz6826/MCrusader");
 	}
 }
--- src/main/java/com/hz6826/mcrusader/mixin/ExampleMixin.java
+++ src/main/java/com/hz6826/mcrusader/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.hz6826.mcrusader.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
